e5ead236719dd9991ae7f4433c3021d85aa04c70
G. Arends <galo@jpresult.nl>
2014-05-13 12:34:21 -0400

    Add Service class

---
+++ src/Service.php
@@ -0,0 +1,44 @@
+<?php
+namespace WMC\MultiPress;
+
+use WMC\MultiPress\Client;
+
+class Service
+{
+    private $connection;
+
+    public function __construct(Client $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function getServerInfo()
+    {
+        return $this->connection->request(0);
+    }
+
+    public function addOrder(\WMC\MultiPress\XML\Order $request)
+    {
+        return $this->connection->request(4, $request->xml->asXML());
+    }
+
+    public function getOrderDetails(\WMC\MultiPress\XML\OrderDetails $request)
+    {
+        return $this->connection->request(11, $request->xml->asXML());
+    }
+
+    public function getInternetBufferStatus(\WMC\MultiPress\XML\InternetBufferStatus $request)
+    {
+        return $this->connection->request(10, $request->xml->asXML());
+    }
+
+    public function getJobNumber(\WMC\MultiPress\XML\InternetBufferStatus $request)
+    {
+        $response = $this->getInternetBufferStatus($request);
+        $processed = (string)$response->INTERNET_BUFFER->PROCESSED;
+        if ($processed == 'JA') {
+          return (string)$response->INTERNET_BUFFER->JOB_NUMBER;
+        }
+        return false;
+    }
+}
