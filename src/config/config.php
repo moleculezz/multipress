b57e23ffc7decf560e8c1f1a138dd55294e8e3e2
G. Arends <galo@jpresult.nl>
2014-05-16 00:10:30 -0400

    Add Laravel ServiceProvider

---
+++ src/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'wsdl' => $_ENV['MULTIPRESS_WSDL'],
+
+    'options' => [
+
+        'login' => $_ENV['MULTIPRESS_LOGIN'],
+
+        'password' => $_ENV['MULTIPRESS_PASSWORD']
+
+    ]
+];
