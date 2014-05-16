b90cc585736313b95d190e5ddea31ee5d1402592
G. Arends <galo@jpresult.nl>
2014-05-16 13:35:09 -0400

    Add ServiceProvider

---
+++ src/MultiPress/MultiPressServiceProvider.php
@@ -0,0 +1,48 @@
+<?php namespace WMC\MultiPress;
+
+use Illuminate\Support\ServiceProvider;
+
+class MultiPressServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('wmc/multipress');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['multipress'] = $this->app->share(function ($app) {
+            $conn = new Client($app['config']['wmc/multipress::wsdl'], $app['config']['wmc/multipress::options']);
+
+            return new Service($conn);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+}
